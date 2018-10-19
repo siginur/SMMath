df870e5d8eb8fb4372fefe86a2a2ede926dd57ac
Alexey Siginur <siginur@gmail.com>
2018-10-19 07:41:14 +0300

    Initial Commit

---
+++ SMMath/SMMath.h
@@ -0,0 +1,19 @@
+//
+//  SMMath.h
+//  SMMath
+//
+//  Created by Alexey Siginur on 19/10/2018.
+//  Copyright © 2018 merkova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SMMath.
+FOUNDATION_EXPORT double SMMathVersionNumber;
+
+//! Project version string for SMMath.
+FOUNDATION_EXPORT const unsigned char SMMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMMath/PublicHeader.h>
+
+
---
+++ SMMathTests/SMMathTests.swift
@@ -0,0 +1,34 @@
+//
+//  SMMathTests.swift
+//  SMMathTests
+//
+//  Created by Alexey Siginur on 19/10/2018.
+//  Copyright © 2018 merkova. All rights reserved.
+//
+
+import XCTest
+@testable import SMMath
+
+class SMMathTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
